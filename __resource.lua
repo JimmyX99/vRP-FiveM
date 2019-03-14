7176955ee3ec03dc14627326c9eef50e0b64223b
Gimi <43321891+XDGimi@users.noreply.github.com>
2019-03-14 09:07:40 +0000

    Add files via upload

---
+++ __resource.lua
@@ -0,0 +1,23 @@
+--[[
+		 The script is releases for free
+		 This is a Youtuber Menu with only Permission
+		 Author:Gimi#0001
+		 All rights reserved
+		 Do not copy the resource without my permission
+		 Only for vRP
+--]]
+
+description "vRP Youtuber Menu"
+
+dependency "vrp"
+
+client_scripts{ 
+	"lib/Tunnel.lua",
+	"lib/Proxy.lua",
+	"client.lua"
+}
+
+server_scripts{ 
+  "@vrp/lib/utils.lua",
+  "server.lua"
+}
