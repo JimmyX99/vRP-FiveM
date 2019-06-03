cdfed57567d48fb9d6a594610073944003ca1d5a
JimmyX <43321891+XDGimi@users.noreply.github.com>
2019-06-03 11:09:04 +0300

    Update client.lua

--- vrp_premium/client.lua
+++ vrp_premium/client.lua
@@ -55,68 +55,7 @@ function vRPCpremium.spawnpremiumCar()
 	end
 end
 
----Sky Fall ---
 
-local isSkyfall = false
-
-function DisplayHelpText(message)
-	SetTextComponentFormat("STRING")
-	AddTextComponentString(message)
-	DisplayHelpTextFromStringLabel(0, 0, 1, -1)
-end
-
-RegisterNetEvent("Skyfall:DoFall")
-AddEventHandler('Skyfall:DoFall', function()
-	if not isSkyfall then
-		isSkyfall = true
-
-		CreateThread(function()
-			local playerPed = PlayerPedId()
-			local playerPos = GetEntityCoords(playerPed)
-
-			GiveWeaponToPed(playerPed, GetHashKey('gadget_parachute'), 1, true, true)
-
-			DoScreenFadeOut(3000)
-
-			while not IsScreenFadedOut() do
-				Wait(0)
-			end
-
-			SetEntityCoords(playerPed, playerPos.x, playerPos.y, playerPos.z + 500.0)
-
-			DoScreenFadeIn(2000)
-
-			Wait(2000)
-
-			DisplayHelpText('Skyfall ~b~activated')
-
-			SetPlayerInvincible(playerPed, true)
-			SetEntityProofs(playerPed, true, true, true, true, true, false, 0, false)
-
-			while true do
-				if isSkyfall then			
-					if IsPedInParachuteFreeFall(playerPed) and not HasEntityCollidedWithAnything(playerPed) then
-						ApplyForceToEntity(playerPed, true, 0.0, 200.0, 2.5, 0.0, 0.0, 0.0, false, true, false, false, false, true)
-					else
-						isSkyfall = false
-					end
-				else
-
-					break
-				end
-
-				Wait(0)
-			end
-
-			RemoveWeaponFromPed(playerPed, GetHashKey('gadget_parachute'))
-
-			Wait(3000)
-
-			SetPlayerInvincible(playerPed, false)
-			SetEntityProofs(playerPed, false, false, false, false, false, false, 0, false)
-		end)
-	end
-end)
 -----------------------------------------------------------------------------------------------------------
 
 Citizen.CreateThread(function()
